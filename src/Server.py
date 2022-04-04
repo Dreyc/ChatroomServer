5747128f63a899855944aa9bcadba178305538de
Dreyc <59511921+Dreyc@users.noreply.github.com>
2022-04-04 11:00:46 +0200

    Username not found Dm fix

--- src/Server.py
+++ src/Server.py
@@ -149,7 +149,7 @@ def handle_client(connection, address):
                             mpConnection.send((f'[DM] {clients[connection]} : ' + message[(len(username)+1):]).encode(FORMAT))
                             connection.send((f'[DM] {clients[connection]} : ' + message).encode(FORMAT))
                         else:
-                            connection.send((f'[{username} NOT FOUND]\nMessage : ' + message[(len(username)+1):]).encode(FORMAT))
+                            connection.send((f'[ {username[1:]} NOT FOUND]\nMessage : ' + message[(len(username)+1):]).encode(FORMAT))
                 else:
                     connection.send("[EMPTY DM]".encode(FORMAT))
             # Anonymous Direct Messages
@@ -166,7 +166,7 @@ def handle_client(connection, address):
                             mpConnection.send((f'[DM] ' + message[(len(username)+1):]).encode(FORMAT))
                             connection.send((f'[DM] {clients[connection]} : ' + message).encode(FORMAT))
                         else:
-                            connection.send((f'[{username} NOT FOUND]\nMessage : ' + message[(len(username)+1):]).encode(FORMAT))
+                            connection.send((f'[ {username[2:]} NOT FOUND]\nMessage : ' + message[(len(username)+1):]).encode(FORMAT))
                 else:
                     connection.send("[EMPTY DM]".encode(FORMAT))
             # Message to end the connection of a client who has been kciked
